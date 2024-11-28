339dc9ab19a3b9f3846651d376e8554139246886
robertinho33 <sou_roberto@hotmail.com>
2024-11-28 10:32:39 -0300

    processar_formulario.php

--- processar_formulario.php
+++ processar_formulario.php
@@ -1,31 +1,45 @@
 <?php
 // Dados de conexão com o banco de dados
 $host = 'localhost';
-$dbname = 'alpha_db';
-$user = 'root';
-$password = '';
+$user = 'root'; // Nome de usuário correto para o XAMPP por padrão
+$password = ''; // Senha padrão do usuário root no XAMPP (geralmente vazia)
+$dbname = 'alpha_db'; // Nome do banco de dados correto
 
-try {
-    // Conexão com o banco de dados usando PDO
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Cria a conexão
+$connex = new mysqli($host, $user, $password, $dbname);
 
-    // Verificar se os dados foram enviados pelo formulário
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $nome = $_POST['nome'];
-        $email = $_POST['instagram'];
-
-        // Inserir os dados na tabela
-        $sql = "INSERT INTO usuarios (nome, instagram) VALUES (:nome, :instagram)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':nome', $nome);
-        $stmt->bindParam(':instagram', $email);
-
-        $stmt->execute();
-
-        echo "Dados inseridos com sucesso!";
-    }
-} catch (PDOException $e) {
-    echo "Erro ao conectar com o banco de dados: " . $e->getMessage();
+// Verifica se houve algum erro na conexão
+if ($connex->connect_errno) {
+    echo "Erro: " . $connex->connect_error;
+} else {
+    echo "Certo";
 }
+
+/*
+ * Descrição: Método responsável por 
+ * 
+ 
+*try {
+    * Conexão com o banco de dados usando PDO
+    *$conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    *$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    * Verificar se os dados foram enviados pelo formulário
+   * if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    *    $nome = $_POST['nome'];
+       * $email = $_POST['instagram'];
+*
+    *    // Inserir os dados na tabela
+    *    $sql = "INSERT INTO usuarios (nome, instagram) VALUES (:nome, :instagram)";
+    *    $stmt = $conn->prepare($sql);
+    *    $stmt->bindParam(':nome', $nome);
+    *    $stmt->bindParam(':instagram', $email);
+*
+    *    $stmt->execute();
+*
+       * echo "Dados inseridos com sucesso!";
+   * }
+*} catch (PDOException $e) {
+    *echo "Erro ao conectar com o banco de dados: " . $e->getMessage();
+*}*/
 ?>
